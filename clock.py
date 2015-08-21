6ecec5a37c90d0a1b7e9f598951895dac34f214b
Rodrigo Deodoro <rodrigo.deodoro@concretesolutions.com.br>
2015-08-21 18:25:29 -0300

    soc + string formatting

--- clock.py
+++ clock.py
@@ -14,6 +14,7 @@ def commit(obj):
     session.add(obj)
     session.commit()
 
+
 def get_today():
     today = session.query(WorkDay).filter(WorkDay.day==datetime.date.today())
 
@@ -22,6 +23,7 @@ def get_today():
     except NoResultFound:
         commit(WorkDay(day=datetime.date.today()))
         return get_today()
+
 
 def insert_event(event):
     events = {
@@ -34,10 +36,33 @@ def insert_event(event):
     setattr(today, events[event], datetime.datetime.now())
     commit(today)
 
+
+def format_time(time):
+    return time.strftime('%H:%M:%S')
+
+def format_timedelta(td):
+    return str(td).split('.')[0]
+
+
+def print_times(day):
+    print "enter:", format_time(day.am_enter) if day.am_enter else ''
+    print "lunch:", format_time(day.am_leave) if day.am_leave else ''
+    print "back: ", format_time(day.pm_enter) if day.pm_enter else ''
+    print "leave:", format_time(day.pm_leave) if day.pm_leave else ''
+    print
+    now = datetime.datetime.now()
+    workday = now - day.am_enter
+    if day.am_leave and day.pm_enter:
+        lunch = day.pm_enter - day.am_leave
+        workday -= lunch
+    print 'worked hours:', format_timedelta(workday)
+    print 'time left:', format_timedelta(datetime.timedelta(hours=8) - workday)
+
+
 if __name__ == '__main__':
     today = get_today()
     if len(sys.argv) == 1:
-        today.print_times()
+        print_times(today)
     else:
         insert_event(sys.argv[1])
-        today.print_times()
+        print_times(today)
--- models.py
+++ models.py
@@ -16,20 +16,6 @@ class WorkDay(Base):
     pm_enter = Column(types.DateTime())
     pm_leave = Column(types.DateTime())
 
-    def print_times(self):
-        print "enter:", self.am_enter or ''
-        print "lunch:", self.am_leave or ''
-        print "back: ", self.pm_enter or ''
-        print "leave:", self.pm_leave or ''
-        print
-        now = datetime.datetime.now()
-        workday = now - self.am_enter
-        if self.am_leave and self.pm_enter:
-            lunch = self.pm_enter - self.am_leave
-            workday -= lunch
-        print 'worked hours:', workday
-        print 'time left:', datetime.timedelta(hours=8) - workday
-
 engine = create_engine('sqlite:///workday.db')
 
 Base.metadata.create_all(engine)
