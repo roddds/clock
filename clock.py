2f8ac852ead23eb47e94220fa5b4b561e93d4bb4
Rodrigo Deodoro <rodrigo.deodoro@concretesolutions.com.br>
2015-08-21 20:06:42 -0300

    avoiding IndexError when len(sys.argv) == 1

--- clock.py
+++ clock.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
     today = get_today()
     wd = print_times(today)
 
-    if sys.argv[1] in EVENTS:
+    if len(sys.argv) > 1 and sys.argv[1] in EVENTS:
         insert_event(sys.argv[1])
 
     elif 'watch' in sys.argv:
